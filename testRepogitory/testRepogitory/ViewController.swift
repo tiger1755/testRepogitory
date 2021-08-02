3e062d2fb3d4404207592e074a96ea85a4c55472
MasayaFujioka <swift.development.masaya@gmail.com>
2021-08-02 23:00:32 +0900

    新規のプロジェクトファイルを作成しました。

---
+++ testRepogitory/testRepogitory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testRepogitory
+//
+//  Created by MasayaFujioka on 2021/08/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
